d443b31ad1ea155d5de6b1fb3e96a3c342a7e5de
AndyTargino <andytargino@outlook.com>
2024-03-28 23:29:46 +0000

    Synchronized with upstream and applied custom changes

--- src/Client.js
+++ src/Client.js
@@ -104,6 +104,18 @@ this.pupPage = page;
         await this.authStrategy.afterBrowserInitialized();
         await this.initWebVersionCache();
 
+        // ocVesion (isOfficialClient patch)
+        await page.evaluateOnNewDocument(() => {
+            const originalError = Error;
+            //eslint-disable-next-line no-global-assign
+            Error = function (message) {
+                const error = new originalError(message);
+                const originalStack = error.stack;
+                if (error.stack.includes('moduleRaid')) error.stack = originalStack + '\n    at https://web.whatsapp.com/vendors~lazy_loaded_low_priority_components.05e98054dbd60f980427.js:2:44';
+                return error;
+            };
+        });
+        
         await page.goto(WhatsWebURL, {
             waitUntil: 'load',
             timeout: 0,
